e6a26f7ddefc41fd1287a76b838327d1912d3229
dxve <57227307+dxve-byte@users.noreply.github.com>
2020-02-18 18:37:31 +0100

    Update internetcrab.py

--- speech/internetcrab.py
+++ speech/internetcrab.py
@@ -1,12 +1,3 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-
-# WETTER:
-
-r = requests.get("https://www.wetter.com/deutschland/*ort*/*kennnummer*.html")
-soup = BeautifulSoup(r.text, 'html.parser')
-str1 = soup.find("p", class_="json-ld-answer")
-for s in str1:
-	iweather = re.sub('<[^>]*>', '', s)
-	#print(iweather)
