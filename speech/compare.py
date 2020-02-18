2a96ab1cadecd5dc0a82d07c9c031c69e62f4f38
dxve <57227307+dxve-byte@users.noreply.github.com>
2020-02-18 11:44:34 +0100

    Create compare.py

---
+++ speech/compare.py
@@ -0,0 +1,24 @@
+'''
+Das ist die "Compare File" um bestimmte Stichwörter zu Filtern damit die Sprachsteuerung
+weiß was Sie damit auswerten soll
+
+!!!ALLES KLEIN SCHREIBEN!!!
+
+'''
+
+welcomegroup = ['hallo', 'hey', 'hi', 'guten tag', 'servus', 'ahoi', 'Huhu', 'moin', 'grüß dich', 'hallöchen', 'na',]			
+				
+timegroup = ['was', 'wieviel', 'wie', 'zeit', 'uhrzeit', 'uhr', 'clock', 'spät']
+
+weathergroup = ['was', 'wie', 'sagt', 'wetter', 'wird', 'klima', 'wird']
+
+howareyougroup = ['wie', "geht's", 'es', 'dir', 'gehts', 'sag']
+
+whereareyougroup = ['wer', 'bist', 'du', 'denn', 'stell', 'dich', 'mal', 'vor']
+
+insultgroup = ['spasst', 'huhrensohn', 'hitler', 'neger', 'dumme', 'sau', 'bitch', 'dünnbrettbohrer', 'blöd',
+				'poltergeist', 'hasse', 'dich', 'du']
+				
+sleeptruegroup = ['sleepmode','schlafmode','schlafmodus', 'ruhemodus', 'ein','anschalten']
+
+sleepfalsegroup = ['sleepmode','schlafmode','schlafmodus', 'ruhemodus', 'aus','ausschalten']
